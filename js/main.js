dec3be014822e14c1a9eb9478524cb12663dfffd
lamindus <lamindus.97@gmail.com>
2020-08-08 11:45:31 +0300

    update info block and create block slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+    $('.photo__slider').slick({
+        prevArrow: '<button class="slick-prev">Previous</button>',
+        nextArrow: '<button class="slick-next">Next</button>',
+    }
+    );
 });
