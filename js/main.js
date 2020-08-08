c566dac9cb5f98243d3b2708a357714ae5af291b
lamindus <lamindus.97@gmail.com>
2020-08-08 15:13:03 +0300

    unrelated histories

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 $(function(){
-<<<<<<< HEAD
     $('.photo__slider').slick({
         prevArrow: '<button class="slick-prev"><img src="images/prev.svg"></button>',
         nextArrow: '<button class="slick-next"><img src="images/next.svg"></button>',
     }
     );
-=======
-
->>>>>>> a13ad62622e8c52f55b0882a1a5c174d47d27ed6
 });
