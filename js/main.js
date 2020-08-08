8ff9669c208a9b1373ea734509b96be83b4c4f6f
lamindus <lamindus.97@gmail.com>
2020-08-08 19:49:26 +0300

    site is finished for all devices

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(function(){
     $('.photo__slider').slick({
         prevArrow: '<button class="slick-prev"><img src="images/prev.svg"></button>',
         nextArrow: '<button class="slick-next"><img src="images/next.svg"></button>',
-    }
-    );
+    });
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
 });
